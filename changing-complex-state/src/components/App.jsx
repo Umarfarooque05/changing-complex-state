2398548c6956e881ec35563b001fe56a254163e0
Umar Farooque <80164102+Umarfarooque05@users.noreply.github.com>
2022-04-15 12:43:08 -0700

    Add files via upload

---
+++ changing-complex-state/src/components/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+function App() {
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    const inputName = event.target.name;
+
+    setFullName((prevValue) => {
+      if (inputName === "fName") {
+        return {
+          fName: newValue,
+          lName: prevValue.lName
+        };
+      } else if (inputName === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: newValue
+        };
+      }
+    });
+  }
+  return (
+    <div className="container">
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
+      <form>
+        <input
+          onChange={handleChange}
+          value={fullName.fName}
+          name="fName"
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleChange}
+          value={fullName.lName}
+          name="lName"
+          placeholder="Last Name"
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
